80524c00b65ce0ce4582bc31ff5dc0fcadb83623
kevin21018212 <mmb2003@iastate.edu>
2024-01-02 22:13:04 -0600

    privacy policy and tos

---
+++ app/privacy/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const PrivacyPolicyPage = () => {
+  return (
+    <div>
+      <h2>Privacy Policy</h2>
+      <p>
+        User Provided Information: We may collect information you provide when
+        using our app, such as registration details and user-generated content.
+        Automatically Collected Information: We may automatically collect
+        certain information, including device information, usage data, and
+        location informat
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPolicyPage;
---
+++ app/tos/page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const TermsOfServicePage = () => {
+  return (
+    <div>
+      <h2>Terms of Service</h2>
+      <p>
+        Terms of Service 1. Acceptance of Terms By accessing or using our app,
+        you agree to these Terms of Service. 2. Use of the App You agree to use
+        the app in compliance with applicable laws and regulations. Any
+        unauthorized use may violate these terms. 3. User Conduct Users are
+        responsible for their actions while using the app. Prohibited activities
+        include, but are not limited to, engaging in illegal activities or
+        violating others' rights. 4. Intellectual Property All content and
+        materials available in the app are protected by intellectual property
+        laws. Users may not copy, reproduce, distribute, or create derivative
+        works without permission. 5. Termination We reserve the right to
+        terminate or suspend user accounts without notice for any reason. 6.
+        Disclaimer of Warranties The app is provided "as is" without warranties
+        of any kind, either expressed or implied. 7. Limitation of Liability We
+        are not liable for any indirect, incidental, special, consequential, or
+        punitive damages. 8. Changes to Terms We may update these Terms of
+        Service, and users are responsible for regularly reviewing them.
+      </p>
+    </div>
+  );
+};
+
+export default TermsOfServicePage;
