39ffb2dd4012727ff103d58ed39a09ee42c271b2
kevin21018212 <mmb2003@iastate.edu>
2024-01-17 16:02:42 -0600

    movie api update

--- app/api/add/movie/route.ts
+++ app/api/add/movie/route.ts
@@ -6,34 +6,36 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
-    const movie_title = searchParams.get("title") as string;
+    const movieTitle = searchParams.get("title") as string;
     const week = parseInt(searchParams.get("week") as string);
 
     // Read API key from environment variables
-    const omdbApiKey = process.env.MOVIE_KEY;
+    const tmdbApiKey = process.env.MOVIE_KEY;
 
-    // Make API call to OMDB to get the movie details
-    const omdbApiUrl = `https://www.omdbapi.com/?t=${encodeURIComponent(
-      movie_title
-    )}&apikey=${omdbApiKey}`;
-    const omdbApiResponse = await fetch(omdbApiUrl);
-    const omdbData = await omdbApiResponse.json();
+    // Make API call to TMDb to search for the movie
+    const tmdbApiUrl = `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
+      movieTitle
+    )}&include_adult=true&language=en-US&page=1&api_key=${tmdbApiKey}`;
+    const tmdbApiResponse = await fetch(tmdbApiUrl);
+    const tmdbData = await tmdbApiResponse.json();
 
-    const url = omdbData.Poster || "";
-
+    // Extract the poster path of the first result
+    const posterPath = tmdbData.results[0]?.poster_path || "";
+    const imgurl = `https://image.tmdb.org/t/p/original${posterPath}`;
+    // Check if the movie already exists in the database
     const movieExists = await db
       .select()
       .from(Movies)
-      .where(eq(Movies.movie_title, movie_title));
+      .where(eq(Movies.movie_title, movieTitle));
 
     if (movieExists.length === 0) {
       await db.insert(Movies).values({
-        movie_title,
-        url,
+        movie_title: movieTitle,
+        url: imgurl,
         week,
       });
     } else {
-      console.log(`Movie with title ${movie_title} already exists.`);
+      console.log(`Movie with title ${movieTitle} already exists.`);
     }
 
     const movies = await db.select().from(Movies);
